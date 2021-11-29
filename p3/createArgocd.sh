b142b8e2687b3adfe19c36837673f94ad56bf8c0
Leonid <58975913+Aenstein@users.noreply.github.com>
2021-11-29 22:35:37 +0300

    Update createArgocd.sh

--- p3/createArgocd.sh
+++ p3/createArgocd.sh
@@ -1,3 +1,31 @@
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
+
+systemctl enable docker.service
+systemctl enable containerd.service
+
+systemctl start docker
+
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubectl
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
 k3d cluster create my-cluster -p "8888:30080"
 
 kubectl create namespace argocd
